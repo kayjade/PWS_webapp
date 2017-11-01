00d36ec305cfe811f07c6f331195f30c1f28def1
Jing_Xu <kayjade519@gmail.com>
2017-11-01 16:17:18 -0400

    Use django template for mode1-3

--- src/playwithsound/urls.py
+++ src/playwithsound/urls.py
@@ -4,4 +4,7 @@ from playwithsound.views import *
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^what-you-sing-is-what-you-see', mode_1, name='mode_1'),
+    url(r'^it-takes-a-song-to-be-an-artist', mode_2, name='mode_2'),
+    url(r'^is-this-what-you-want', mode_3, name='mode_3'),
 ]
--- src/playwithsound/views.py
+++ src/playwithsound/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def home(request):
     context={}
     return render(request, 'index.html', context)
+
+def mode_1(request):
+    context={}
+    return render(request, 'modes/mode1.html', context)
+
+def mode_2(request):
+    context={}
+    return render(request, 'modes/mode2.html', context)
+
+def mode_3(request):
+    context={}
+    return render(request, 'modes/mode3.html', context)
