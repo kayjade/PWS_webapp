627c4aaa1371b7b67e570f336f9d6a9eba95d3af
Jing_Xu <kayjade519@gmail.com>
2017-11-04 19:43:01 -0400

    Extract data from audio stream for RT process

--- src/playwithsound/static/playwithsound/js/mode1.js
+++ src/playwithsound/static/playwithsound/js/mode1.js
@@ -1,21 +1,68 @@
-var record="0";
-
 $(document).ready(function() {
+  var record="0";
+
+  navigator.getUserMedia = (navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia);
+
+  var audioCtx;
+  var bufferSize=1024;
+  
   $("#start-record").click( function() {
   	if(record=="0"){
-  	  $(".mode-body p").html("Recording now...");	
+  	  $("#record-states").html("Recording now...");	
   	  $("#ongoing-record").css("color","#b4d5c4");
   	  record="1";
+
+      audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+      var source;
+      var stream;
+
+      var displayNode = audioCtx.createScriptProcessor(bufferSize, 1, 1);
+      displayNode.onaudioprocess = function(e) {
+        var input = e.inputBuffer.getChannelData(0);
+        var output = e.outputBuffer.getChannelData(0);
+        var outputStr = "";
+        for (var i = 0; i < bufferSize; i++) {
+          // Modify the input and send it to the output.
+          output[i] = input[i];
+        }
+        console.log(output);
+      }
+
+      var gainNode = audioCtx.createGain();
+      gainNode.gain.value = 0;
+
+      if(navigator.getUserMedia) {
+        console.log('getUserMedia supported.');
+        navigator.getUserMedia (
+          {audio: true},
+          function(stream) {
+            source = audioCtx.createMediaStreamSource(stream);  
+            source.connect(displayNode);
+            displayNode.connect(gainNode);
+            gainNode.connect(audioCtx.destination);
+          },
+          function(err) {
+            console.log('The following gUM error occured: ' + err);
+          }
+        );
+      } else {
+        console.log('getUserMedia not supported on your browser!');
+      }
   	}
   });
 
   $("#stop-record").click( function() {
   	if(record=="1"){
-  	  $(".mode-body p").html("Record ended. Click the button below to start recording again.");	
+  	  $("#record-states").html("Record ended. Click the button below to start recording again.");	
   	  $("#ongoing-record").css("color","#a8505f");
   	  record="0";
+      audioCtx.close();
   	}
   });
+
 });
 
 //demo
