9c7f529cb99196b96f0ea7af32a4246fa79aaebe
Flora van <jingweifan11@yahoo.com>
2017-11-06 02:50:07 -0500

    Add model1 data virtualition mode2

--- src/playwithsound/static/playwithsound/js/model1.js
+++ src/playwithsound/static/playwithsound/js/model1.js
@@ -1,74 +1,100 @@
-function drawpicture(recorder){
-    mode1();
-    function mode1() {
-        var width = 960,
-            height = 500;
+function drawpicture(recorder) {
+    //mode1(recorder);
+    mode2(recorder);
+}
 
-        var canvas = d3.select(".svg").append('canvas')
+function stopdrawpicture() {
+    timer.stop();
+}
+
+function mode1(recorder) {
+    var width = 960,
+        height = 500;
+
+    var canvas = d3.select(".svg").append('canvas')
         .attr("width", width)
         .attr("height", height);
 
-        var x1 = width / 2,
-            y1 = height / 2,
-            i = 0,
-            r = 200,
-            τ = 2 * Math.PI;
+    var x1 = width / 2,
+        y1 = height / 2,
+        i = 0,
+        r = 200,
+        τ = 2 * Math.PI;
 
-        var context = canvas.node().getContext("2d");
-        context.globalCompositeOperation = "source-over";
-        context.lineWidth = 2;
+    var context = canvas.node().getContext("2d");
+    context.globalCompositeOperation = "source-over";
+    context.lineWidth = 2;
 
-        timer=d3.timer(function () {
-            //context.clearRect(0, 0, width, height);
+    timer = d3.timer(function () {
+        //context.clearRect(0, 0, width, height);
 
-            var z = d3.hsl(++i % 360, 1, .5).rgb(),
-                c = "rgba(" + z.r + "," + z.g + "," + z.b + ",",
-                x = x1 ,
-                y = y1 ;
+        var z = d3.hsl(++i % 360, 1, .5).rgb(),
+            c = "rgba(" + z.r + "," + z.g + "," + z.b + ",",
+            x = x1,
+            y = y1;
 
-            d3.select({}).transition()
-                .duration(500)
-                .ease(Math.sqrt)
-                .tween("circle", function () {
-                    return function (t) {
-                        context.strokeStyle = c + (1 - t) + ")";
-                        context.beginPath();
-                        context.arc(x, y, r * t, 0, τ);
-                        context.stroke();
-                    };
-                });
-        });
+        d3.select({}).transition()
+            .duration(500)
+            .ease(Math.sqrt)
+            .tween("circle", function () {
+                return function (t) {
+                    context.strokeStyle = c + (1 - t) + ")";
+                    context.beginPath();
+                    context.arc(x, y, r * t, 0, τ);
+                    context.stroke();
+                };
+            });
+    });
 
-        function getdata() {
-            TD = recorder.timeData;
-            FD = recorder.freqData;
-            r = Math.min(d3.mean(FD)*4,200);
-            x1 =width*Math.random();
-            y1 = height*Math.random();
-        }
-        window.setInterval(getdata, 1000);
+    function getdata() {
+        TD = recorder.timeData;
+        FD = recorder.freqData;
+        r = Math.min(d3.mean(FD) * 4, 200);
+        x1 = width * Math.random();
+        y1 = height * Math.random();
     }
 
+    window.setInterval(getdata, 1000);
+}
 
-    function mode2() {
+
+function mode2(recorder) {
+
+    function getdata() {
+        TD = recorder.timeData;
+        FD = recorder.freqData;
+        CTD = recorder.convTimeData;
+        CFD = recorder.convFreqData;
+    }
+
+    time = 1;
+    window.setInterval(getdata, 1000);
+    window.setInterval(area, 1000);
+
+    var width = 960,
+        height = 500;
+    var svg = d3.select(".svg").append('svg')
+        .attr("width", width)
+        .attr("height", height);
+
+    var a = [], b = [], c = [], d = [], i;
+    for (i = 0; i < 200; ++i){
+        a[i] = 0;
+        b[i] = 0;
+        c[i] = 0;
+        d[i] = 0;
+    }
+    function area() {
         var n = 20, // number of layers
             m = 200, // number of samples per layer
             k = 10; // number of bumps per layer
-        var svg = d3.select(".svg").append('svg'),
-        width = +svg.attr("width"),
-        height = +svg.attr("height");
+
         var stack = d3.stack()
                 .keys(d3.range(n))
                 .offset(d3.stackOffsetWiggle),
-            layers0 = stack(d3.transpose(d3.range(n).map(function () {
-                return bumps(m, k);
-            }))),
-            layers1 = stack(d3.transpose(d3.range(n).map(function () {
-                return bumps(m, k);
-            }))),
-            layers = layers0.concat(layers1);
-        console.log(layers0);
-
+            layers = stack(d3.transpose(d3.range(n).map(function (ln) {
+                return bumps(ln, time);
+            })));
         var x = d3.scaleLinear()
             .domain([0, m - 1])
             .range([0, width]);
@@ -91,13 +117,15 @@ function drawpicture(recorder){
                 return y(d[1]);
             });
 
+        $('svg').empty()
         svg.selectAll("path")
-            .data(layers0)
+            .data(layers)
             .enter().append("path")
             .attr("d", area)
             .attr("fill", function () {
                 return z(Math.random());
             });
+        time++;
     }
 
     function stackMax(layer) {
@@ -112,35 +140,24 @@ function drawpicture(recorder){
         });
     }
 
-    function transition() {
-        var t;
-        d3.selectAll("path")
-            .data((t = layers1, layers1 = layers0, layers0 = t))
-            .transition()
-            .duration(2500)
-            .attr("d", area);
-    }
 
 // Inspired by Lee Byron’s test data generator.
-    function bumps(n, m) {
-        var a = [], i;
-        for (i = 0; i < n; ++i) a[i] = 0;
-        for (i = 0; i < m; ++i) bump(a, n);
-        return a;
-    }
-
-    function bump(a, n) {
-        var x = 1 / (0.1 + Math.random()),
-            y = 2 * Math.random() - 0.5,
-            z = 10 / (0.1 + Math.random());
-        for (var i = 0; i < n; i++) {
-            var w = (i / n - y) * z;
-            a[i] += x * Math.exp(-w * w);
+    function bumps(ln, time) {
+        if(ln == 0) {
+            a[time] = d3.mean(FD);
+            return a;
+        } else if(ln == 1) {
+            b[time] = d3.mean(TD);
+            return b;
+        } else if(ln == 2) {
+            c[time] = d3.mean(CFD);
+            return c;
+        } else if(ln >= 3) {
+            d[time] = d3.mean(CTD)*Math.random();
+            return d;
         }
+
     }
 
-}
 
-function stopdrawpicture() {
-    timer.stop();
 }
