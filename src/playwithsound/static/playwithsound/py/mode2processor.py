6257972c94fa554b79030286aa141e01c57f269a
Flora van <jingweifan11@yahoo.com>
2017-12-03 15:28:10 -0500

    K nearest neighbors

--- src/playwithsound/static/playwithsound/py/mode2processor.py
+++ src/playwithsound/static/playwithsound/py/mode2processor.py
@@ -2,14 +2,16 @@
 # import the necessary packages
 from __future__ import print_function
 import cv2
-from Tkinter import *
 import wave as wav
 import numpy as np
-import math
 import random
 from matplotlib import pyplot as plt
+from scipy import misc
+import knntrain
 
-
+w=160
+h=125
+###Read and transform wav data###
 def read_wav_data(file_path):
     f=wav.open(file_path,"rb")
     params=f.getparams()
@@ -22,69 +24,65 @@ def read_wav_data(file_path):
     time = np.arange(0, nframes) * (1.0 / framerate)
     return wav_data,time
 
-def transform_data(data):
+def transform_classify(data):
     data=np.asarray(data)
     data=np.ndarray.flatten(data)
-    figure=np.random.randint(0,5,[250,320])
+    figure=np.zeros([h,w])
+    figure=np.add(figure,127)
     l=len(data)
-    l=int(round(l/320))
-    if l>250:
-        l=250
+    l=int(round(l/w))
+    if l>h:
+        l=h
 
     for i in range(0,l):
-            figure[i]=data[i*320:(i+1)*320:1]
-
-    return figure
-
-def main():
-    path="/Users/flora/Documents/CMU/CLASSES/15637Web/Team330/src/media/music/2017-12-02T19_55_28.929Z.wav"
-    wav_data,time=read_wav_data(path)
-    size=wav_data.shape[1]
-
-    length=int(round(960*500/3,0))
-    left_data=wav_data[0]
-    left_data=systematic_sampling(left_data,960*500)
-    right_data=wav_data[1]
-    right_data=systematic_sampling(right_data, 960 * 500)
-
-    np.compress([0,0,],left_data)
-    l1=left_data[0:length-1:1]
-    l2=left_data[length:2*length-1:1]
-    l3=left_data[length*2::1]
-    r1 = right_data[0:length - 1:1]
-    r2 = right_data[length:2 * length - 1:1]
-    r3 = right_data[length * 2::1]
-
-    p1=classify(l1)
-    p1=classify(l2)
-    p3=classify(l3)
-    p4=classify(r1)
-    p5=classify(r2)
-    p6=classify(r3)
+            figure[i]=data[i*w:(i+1)*w:1]
+    figure=figure.astype(int)
+    return knntrain.main(figure)
 
 def classify(data):
-    transform_data(data)
-    # load the image and convert it to grayscale
-    image = cv2.imread("carton.jpg")
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Original", image)
+    data=data.astype(int)
+    data = norm_range(data,min(data)[0],max(data)[0],0,255)
+    length = w * h
+    d1=data[0:length-1:1]
+    d2=data[length:2*length-1:1]
+    d3=data[length*2::1]
+    p1=transform_classify(d1)
+    p2=transform_classify(d2)
+    p3=transform_classify(d3)
+    img1=cv2.imread(p1)
+    img2=cv2.imread(p2)
+    img3=cv2.imread(p3)
+    merge=np.hstack((img1,img2,img3))
+    return merge
 
-    # initialize the AKAZE descriptor, then detect keypoints and extract
-    # local invariant descriptors from the image
-    detector = cv2.AKAZE_create()
-    (kps, descs) = detector.detectAndCompute(gray, None)
-    print("keypoints: {}, descriptors: {}".format(len(kps), descs.shape))
-
-    # draw the keypoints and show the output image
-    cv2.drawKeypoints(image, kps, image, (0, 255, 0))
-    cv2.imshow("Output", image)
-    cv2.waitKey(0)
-
+def norm_range(data,lmin,lmax,rmin,rmax):
+    scale=float(rmax-rmin)/float(lmax-lmin)
+    data=data*scale
+    data=data+(rmax-rmin)/2
+    data=data.astype(int)
+    return data
 
 def systematic_sampling(dataMat, num):
     k = int(len(dataMat)/num)
     samples = [random.sample(dataMat[i*k:(i+1)*k], 1) for i in range(num)]
-    return samples
+    return np.array(samples)
+
+def main(path="/Users/flora/Documents/CMU/CLASSES/15637Web/Team330/src/media/music/2017-12-02T19_55_28.929Z.wav"):
+    wav_data,time=read_wav_data(path)
+
+    left_data=wav_data[0]
+    left_data=systematic_sampling(left_data,3*w * h)
+
+    right_data=wav_data[1]
+    right_data=systematic_sampling(right_data,3*w * h)
+    p1=classify(left_data)
+    p2=classify(right_data)
+    vmerge = np.vstack((p1, p2))
+    plt.imshow(vmerge), plt.show()
+    p='../../../../media/mode2/'+path.split('.',1)[0]+'png'
+    misc.toimage(vmerge, cmin=0, cmax=255).save(p)
+    return p
 
 if __name__ == "__main__":
     main()
+
--- src/playwithsound/views.py
+++ src/playwithsound/views.py
@@ -20,7 +20,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_text
-
+import
 import os
 
 # Create your views here.
